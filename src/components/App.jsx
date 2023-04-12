fe6601a312f005b267074566bb28154a4cb042c4
selene-cho <selene.cho29@gmail.com>
2023-04-13 02:03:34 +0900

    feat: pagination

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,13 @@ import { getReviews } from '../api';
 import ReviewList from './ReviewList';
 import { useEffect, useState } from 'react';
 
+const LIMIT = 6;
+
 export default function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState('createdAt');
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder('createdAt');
@@ -13,13 +17,22 @@ export default function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-  const handleLoad = async (orderQuery) => {
-    const { reviews } = await getReviews(orderQuery);
-    setItems(reviews);
+  const handleLoad = async (options) => {
+    const { reviews, paging } = await getReviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
   };
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
 
   return (
@@ -29,6 +42,9 @@ export default function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button disabled={!hasNext} onClick={handleLoadMore}>
+        더보기
+      </button>
     </div>
   );
 }
