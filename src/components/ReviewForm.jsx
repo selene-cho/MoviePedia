3e9bcaec7bafd39441534bb03b7577e45cadea03
selene-cho <selene.cho29@gmail.com>
2023-05-22 03:06:29 +0900

    feat: 하나의 state로 표현

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -2,30 +2,35 @@ import { useState } from 'react';
 import './ReviewForm.css';
 
 export default function ReviewForm() {
-  const [title, setTitle] = useState('');
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState('');
+  const [values, setValues] = useState({
+    title: '',
+    rating: 0,
+    content: '',
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+      //[대괄호] name의 값으로 프로퍼티 명을 지정하고 해당하는 값 지정 가능
+    }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, rating, content);
+    console.log(values);
   };
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        type="number"
+        name="rating"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
       <button type="submit">확인</button>
     </form>
   );
