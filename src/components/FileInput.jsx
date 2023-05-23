cbb130763b3cf5d6a8218901494ac33a07faf6d7
selene-cho <selene.cho29@gmail.com>
2023-05-23 21:08:14 +0900

    파일 input 초기화

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -1,16 +1,25 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
-export default function FileInput({ name, onChange, type }) {
+export default function FileInput({ name, onChange, type, value }) {
   const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue, type);
   };
-  useEffect(() => {
-    if (inputRef.current) {
-      console.log(inputRef.current);
-    }
-  }, []);
 
-  return <input type={type} onChange={handleChange} ref={inputRef} />;
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = '';
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type={type} onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
 }
