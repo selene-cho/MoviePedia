7653c7c601fb2e789a1d7f3510840b77547b83cf
selene-cho <selene.cho29@gmail.com>
2023-05-24 00:58:46 +0900

    사이드 이펙트 정리

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -22,12 +22,23 @@ export default function FileInput({ name, onChange, type, value }) {
 
     const nextPreview = URL.createObjectURL(value);
     setPreview(nextPreview);
+
+    return () => {
+      setPreview(); // preview state 빈값으로 만듦
+      URL.revokeObjectURL(nextPreview); // 사이드 이펙트 정리
+      // 리턴값 기억해뒀다가 다음 value state 변경될 때 실행, ObjectURL 더이상 사용하지 않으니까 정리
+    };
   }, [value]); // 파일 선택할 때마다 미리보기 주소 변경
 
   return (
     <div>
       <img src={preview} alt="이미지 미리보기" />
-      <input type={type} onChange={handleChange} ref={inputRef} />
+      <input
+        type={type}
+        accept="image/png, image/jpeg"
+        onChange={handleChange}
+        ref={inputRef}
+      />
       {value && <button onClick={handleClearClick}>X</button>}
     </div>
   );
