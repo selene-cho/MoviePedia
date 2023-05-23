7526d3840c6f80641923f0bd6bc9a2d2c1da04b6
selene-cho <selene.cho29@gmail.com>
2023-05-24 00:49:03 +0900

    이미지 파일 미리보기

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -1,6 +1,7 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function FileInput({ name, onChange, type, value }) {
+  const [preview, setPreview] = useState(); // 파일 미리보기 주소를 문자열로 저장
   const inputRef = useRef();
 
   const handleChange = (e) => {
@@ -16,8 +17,16 @@ export default function FileInput({ name, onChange, type, value }) {
     onChange(name, null);
   };
 
+  useEffect(() => {
+    if (!value) return; // 파일 없을 때, useEffect 종료
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+  }, [value]); // 파일 선택할 때마다 미리보기 주소 변경
+
   return (
     <div>
+      <img src={preview} alt="이미지 미리보기" />
       <input type={type} onChange={handleChange} ref={inputRef} />
       {value && <button onClick={handleClearClick}>X</button>}
     </div>
