8a1707ffe9c05fcadab17ff3968b1aeb5137ab59
selene-cho <selene.cho29@gmail.com>
2023-05-25 00:06:00 +0900

    4. 데이터 보내기/ 1 - 글 작성하기

--- src/api.js
+++ src/api.js
@@ -2,6 +2,8 @@
  * request 함수(API 요청) 모아 놓은 파일
  */
 
+const BASE_URL = 'https://learn.codeit.kr/0529';
+
 // Review 가져오는 함수
 export async function getReviews({
   order = 'createdAt',
@@ -9,11 +11,23 @@ export async function getReviews({
   limit = 6,
 }) {
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
-  const response = await fetch(
-    `https://learn.codeit.kr/0529/film-reviews?${query}`
-  );
+  const response = await fetch(`${BASE_URL}/film-reviews?${query}`);
   if (!response.ok) {
     throw new Error('리뷰를 불러오는데 실패했습니다');
+  }
+  const body = await response.json();
+  return body;
+}
+
+// Review 생성하는 함수
+export async function createReview(formData) {
+  // throw new Error('버그가 아니라 기능입니다'); // 에러메세지 출력 잘 되는지 TEST
+  const response = await fetch(`${BASE_URL}/film-reviews`, {
+    method: 'POST',
+    body: formData,
+  });
+  if (!response.ok) {
+    throw new Error('리뷰를 생성하는데 실패했습니다');
   }
   const body = await response.json();
   return body;
--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react';
 import './ReviewForm.css';
 import FileInput from './FileInput';
 import RatingInput from './RatingInput';
+import { createReview } from '../api';
+
+const INITIAL_VALUES = {
+  title: '',
+  rating: 0,
+  content: '',
+  imgFile: null,
+};
 
 export default function ReviewForm() {
-  const [values, setValues] = useState({
-    title: '',
-    rating: 0,
-    content: '',
-    imgFile: null,
-  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
+  const [values, setValues] = useState(INITIAL_VALUES);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -21,9 +26,24 @@ export default function ReviewForm() {
     const { name, value } = e.target;
     handleChange(name, value);
   };
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append('title', values.title);
+    formData.append('rating', values.rating);
+    formData.append('content', values.content);
+    formData.append('imgFile', values.imgFile);
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      await createReview(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+    setValues(INITIAL_VALUES); // request 끝나면 form 초기화
   };
 
   return (
@@ -44,7 +64,10 @@ export default function ReviewForm() {
         value={values.content}
         onChange={handleInputChange}
       />
-      <button type="submit">확인</button>
+      <button type="submit" disabled={isSubmitting}>
+        확인
+      </button>
+      {submittingError?.message && <div>{submittingError.message}</div>}
     </form>
   );
 }
